bee8e6af1e972510121295831bba208cc528edd3
Your Brennan Predmore <brennnann@gmail.com>
2020-01-28 17:36:49 -0600

    new bulma calendar init with style // 2 city input init

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-    $('#currentDay').append(moment().format('dddd, MMM Do'))
+// Initialize all input of date type.
+const calendars = bulmaCalendar.attach('[type="date"]', options);
 
-    $('#search_btn').click(function () {
-
-     
-
-
-    });
-
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to date:selected event
+	calendar.on('date:selected', date => {
+		console.log(date);
+	});
 });
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
+
+    
+    console.log($('#calendar').val())
+});
+
