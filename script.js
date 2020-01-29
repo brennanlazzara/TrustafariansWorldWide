632163f8d2c62300e5296f9c10ddad74b08d5c2a
Your Brennan Predmore <brennnann@gmail.com>
2020-01-28 20:44:31 -0600

    skyscanner API init

--- script.js
+++ script.js
@@ -8,43 +8,35 @@ $(document).ready(function () {
 
    $("#search").on("click", function(){
 
+    $('#calendar').hide();
+
     let departDest = $('#dDest').val();
     let arriveDest = $('#aDest').val();
     let departDate = $('#dDate').val();
     let arriveDate = $('#aDate').val();
 
-//     var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${departDest}/${arriveDest}/${departDate}?inboundpartialdate=${arriveDate}`,
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-//             "x-rapidapi-key": "0370f76c20msh20d28916440452ap120f23jsnac184da220ba"
-//         }
-//     }
-
-// $.ajax(settings).done(function(response){
-//     console.log(response);
-// }
-// )
-
-var settings = {
+    // Flight Skyscanner API
+var flights = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO-sky/ORD-sky/2020-02-01?inboundpartialdate=2020-02-03",
+	"url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/${departDest}/${arriveDest}/${departDate}?inboundpartialdate=${arriveDate}`,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
 		"x-rapidapi-key": "0370f76c20msh20d28916440452ap120f23jsnac184da220ba"
 	}
 }
-$.ajax(settings).done(function (response) {
-	console.log(response);
+$.ajax(flights).done(function (response) {
+    console.log(response.Quotes);
+    console.log(response.Places);
+    console.log(response.Carriers);
 });
 
 
    });
+
+   // Lyft API
+   
 
 });
 
