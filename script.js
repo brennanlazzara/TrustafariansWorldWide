7483c706eda25fa72695c5d903567769ec1e1ca0
Your Brennan Predmore <brennnann@gmail.com>
2020-01-28 18:08:05 -0600

    header size 100px

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+import bulmaCalendar from '~bulma-calendar/dist/js/bulma-calendar.min.js';
+
 $(document).ready(function () {
-// Initialize all input of date type.
-const calendars = bulmaCalendar.attach('[type="date"]', options);
+    // Initialize all input of date type.
+    const calendars = bulmaCalendar.attach('[type="date"]', options);
 
-// Loop on each calendar initialized
-calendars.forEach(calendar => {
-	// Add listener to date:selected event
-	calendar.on('date:selected', date => {
-		console.log(date);
-	});
+    // Loop on each calendar initialized
+
+    calendars.forEach(calendar => {
+        // Add listener to date:selected event
+        calendar.on('date:selected', date => {
+            console.log(date);
+        });
+    });
+
+    // To access to bulmaCalendar instance of an element
+    const element = document.querySelector('#my-element');
+    if (element) {
+        // bulmaCalendar instance is available as element.bulmaCalendar
+        element.bulmaCalendar.on('select', datepicker => {
+            console.log(datepicker.data.value());
+        });
+    }
+
+    $('#search').on('click', function(){
+        console.log("Whats up dog");
+    })
+
 });
-
-// To access to bulmaCalendar instance of an element
-const element = document.querySelector('#my-element');
-if (element) {
-	// bulmaCalendar instance is available as element.bulmaCalendar
-	element.bulmaCalendar.on('select', datepicker => {
-		console.log(datepicker.data.value());
-	});
-}
-
-    
-    console.log($('#calendar').val())
-});
-
